a54c765dc0c3f7890da5849e8228b9b4a2ce84ed
oh-gnues-iohc <79557937+oh-gnues-iohc@users.noreply.github.com>
2023-10-23 17:28:56 +0900

    Add files via upload

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "Github 블로그 첫걸음 - 블로그 생성",
+        "excerpt":"처음 만들어 보는데 어떻게 하는지 몰라서 이 문서, 저 문서 찾아보면서 하는중 왠지 나 같은 사람들 많을듯해서 정리하면서 만들어봄 1. Github Blog Repository 생성 단순하게 자신의 계정 이름으로 생성한 뒤, 생성한 Repo의 설정 -&gt; Pages에 들어가 사진과 같이 설정해주면 끝 🤔 이름.github.io 로 만들면 더 편한거 아님??? 맞는 말이긴 한데...","categories": ["BLOG"],
+        "tags": ["Blog","Jekyll","Github","Git"],
+        "url": "/blog/blog-01/",
+        "teaser": "https://github-readme-stats.vercel.app/api/pin/?username=oh-gnues-iohc&repo=oh-gnues-iohc.github.io"
+      },{
+        "title": "Github 블로그 첫걸음 - 포스트",
+        "excerpt":"1. 포스트 작성하기 .md 깃허브 블로그는 Markdown이랑 뭐랑 둘 중 하나로 포스트를 한다는데 Markdown 쓸거니 머리속에 넣지도 않아서 기억 안남 말 그대로 내가 작성할 포스트의 내용을 Markdown으로 작성하면 됨 주의할 점은 아래와 같이 md 파일 내에 해당 포스트의 정보를 머릿말로 적어야함 --- title: \"Github 블로그 첫걸음 - 포스트\" excerpt: \"내용...","categories": ["BLOG"],
+        "tags": ["Blog","Jekyll","Github","Git"],
+        "url": "/blog/blog-02/",
+        "teaser": "https://github-readme-stats.vercel.app/api/pin/?username=oh-gnues-iohc&repo=oh-gnues-iohc.github.io"
+      },{
+        "title": "Github 블로그 첫걸음 - minimal-mistakes 꾸미기",
+        "excerpt":"🔔 계속 업데이트 될 예정 Jekyll 한글 문서와 Minimal Mistakes 공식 문서를 참고하였음 크게 어려운건 없는데, 프론트는 깔짝 깔짝 겉할기 한게 전부이다 보니 시간이 좀 걸린다. 그래도 파일명이나 문서 자체가 상당히 직관적이라 생각보다 금방 익히는중 이 분야 잘 아는 사람들은 이 글이 아니라 위 문서 직접 보면서 하는게 더 빠를듯...","categories": ["BLOG"],
+        "tags": ["Blog","Jekyll","Github","Git"],
+        "url": "/blog/blog-03/",
+        "teaser": "https://github-readme-stats.vercel.app/api/pin/?username=oh-gnues-iohc&repo=oh-gnues-iohc.github.io"
+      },{
+        "title": "Github 블로그 첫걸음 - 폰트 변경",
+        "excerpt":"차라리 꾸미는 걸 따로 따로 빼서 글 쓰는게 나을듯 무료 폰트 고르기 눈누 구글 폰트 보통 이렇게 두개 많이 사용하는 듯 나는 눈누에서 주아체 가져왔음 원하는 폰트 보면 웹 폰트로 사용 이라고 있음 이걸 복사해서 임포트하면 끝임 @font-face { font-family: 'BMJUA'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/BMJUA.woff') format('woff'); font-weight: normal; font-style: normal; } 임포트...","categories": ["BLOG"],
+        "tags": ["Blog","Jekyll","Github","Git"],
+        "url": "/blog/blog-04/",
+        "teaser": "https://github-readme-stats.vercel.app/api/pin/?username=oh-gnues-iohc&repo=oh-gnues-iohc.github.io"
+      },{
+        "title": "Github 블로그 첫걸음 - 카테고리 만들기",
+        "excerpt":"너무 맘에 드는 디자인을 잘 정리해준 블로그가 있어 보고 그대로 따라했음 위 블로그에선 여러 파일을 추가해서 진행했는데, 나는 그런게 좀 싫고 원래 가지고 있는 파일에 적용하고 싶어서 그렇게 했음 같은 카테고리 끼리 모으기 이걸 만들어 주는 단계임 minimal-mistakes ㄴ 📁 _pages ㄴ 📁 categories ㄴ 📝 category-카테고리명.md 이렇게 _pages/categories 폴더에...","categories": ["BLOG"],
+        "tags": ["Blog","Jekyll","Github","Git"],
+        "url": "/blog/blog-05/",
+        "teaser": "https://github-readme-stats.vercel.app/api/pin/?username=oh-gnues-iohc&repo=oh-gnues-iohc.github.io"
+      },{
+        "title": "Github 블로그 검색창 노출 시키기 [구글, 네이버]",
+        "excerpt":"블로그를 하면서 광고로 돈을 벌게 아니더라도 누군가 봐주지 않는다면 글을 쓸 필요가 없음 그렇기 때문에 검색에 노출되도록 하는것이 중요 Google Search Console Google Search Console은 말 그대로 구글 검색에 블로그가 노출 될 수 있도록 등록하는 서비스임 시작하기를 누른 뒤, 일반적으론 도메인이 없는 상황이기 때문에 URL 접두어에 블로그의 링크를 걸고 계속을...","categories": ["BLOG"],
+        "tags": ["Blog","Jekyll","Github","Git"],
+        "url": "/blog/search/",
+        "teaser": "https://github-readme-stats.vercel.app/api/pin/?username=oh-gnues-iohc&repo=oh-gnues-iohc.github.io"
+      },{
+        "title": "Huggigface Trainer Arguments Viewer",
+        "excerpt":"GitHub - oh-gnues-iohc/Huggingface-Trainer-Args-Viewer: Code for easy management of HuggingFace Trainer arguments with Streamlit Code for easy management of HuggingFace Trainer arguments with Streamlit - GitHub - oh-gnues-iohc/Huggingface-Trainer-Args-Viewer: Code for easy management of HuggingFace Trainer arguments with Str... github.com 프로젝트 개요 HuggingFace의 Trainer의 인자(Arguments) 관리를 쉽게 하기 위한 프로젝트 개발 이유...","categories": ["TOY PROJECT"],
+        "tags": ["Huggingface","Python","Side Project"],
+        "url": "/toy%20project/huggingface-viewer/",
+        "teaser": "https://github-readme-stats.vercel.app/api/pin/?username=oh-gnues-iohc&repo=Huggingface-Trainer-Args-Viewer"
+      }]
